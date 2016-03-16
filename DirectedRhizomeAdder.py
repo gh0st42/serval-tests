c1e265c79be2e9d23cb658febf822f0779f38955
Artur <github@artursterz.de>
2016-03-16 16:28:02 +0100

    Added timout option.

--- DirectedRhizomeAdder.py
+++ DirectedRhizomeAdder.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description='Periodically generate and add dire
 parser.add_argument('-d', dest='min_delay_ms', default=0, help='set minimum insertion delay (ms)')
 parser.add_argument('-j', dest='delay_jitter_ms', default=1000, help='set maximum jitter (ms) for insertion delay')
 parser.add_argument('-s', dest='size_k', default=1024, help='size (KiB) of files to be inserted')
+parser.add_argument('-t', dest='timeout', default=-1, type=int, help='stop after timout')
 args = parser.parse_args()
 
 # adding parsed /default values to global dict 
@@ -14,6 +15,8 @@ globals().update(vars(args))
 random.seed(socket.gethostname())
 my_sid = getSid()
 neighbours = getNeightbourSids()
+
+later = time.time() + timeout
 
 if __name__ == "__main__":
     count = 0
@@ -23,6 +26,9 @@ if __name__ == "__main__":
     basename = socket.gethostname()
     if len(neighbours) == 0: print("No neighbours found, exiting"); sys.exit(1)
     while running:
+    	if timeout != -1 and later < time.time():
+			running = False
+			break
         their_sid = random.choice(neighbours)
         rhizomeRandomFile(basename+"-"+str(size_k)+"k-"+str(count)+".bin", size_k, my_sid, their_sid=their_sid)
         count += 1
