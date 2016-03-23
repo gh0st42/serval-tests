fb3cda246d9149d40c18d693a59cd487354e06c0
nicer <github@rubyfi.de>
2016-03-23 12:22:25 +0100

    Filter meshms logs for own sid

--- meshms-insertion-log.py
+++ meshms-insertion-log.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
-import os, sys
+import os, sys, subprocess
+
+SERVALD_BIN = "servald"
+
+def getOwnSid():
+    sid = subprocess.check_output(SERVALD_BIN+" id self", shell=True)
+    return "'" + sid.split('\n')[2] + "'"
 
 LINE_FILTERS = ['INSERT OR REPLACE INTO MANIFESTS', 'MeshMS2']
 VALUES_START_STRING='VALUES('
@@ -18,4 +24,6 @@ for filename in os.listdir(sys.argv[1]):
     file = open(sys.argv[1] + "/" + filename, 'r')
     lines = (line for line in file if all(filter in line for filter in LINE_FILTERS))
     for line in lines:
-        print extract_values(line)
+        line_list = line.split('VALUES')[1].replace('(', '').replace(')', '').replace('\n', '').replace(';', '').split(',')
+	if (line_list[-4] == getOwnSid() or line_list[-3] == getOwnSid()):
+	    print ','.join(line_list)
