294ca48523dc45072f2b089fb880bb81bf464fce
adur1990 <github@artursterz.de>
2016-05-20 14:24:15 +0200

    Changed call from cli to restful.

--- meshms-insertion.py
+++ meshms-insertion.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
-import hashlib, subprocess, os, binascii, time, sys
-
-SERVALD_BIN = "servald"
+import hashlib, subprocess, os, binascii, time, sys, json
 
 def getOwnSid():
-    sid = subprocess.check_output(SERVALD_BIN+" id self", shell=True)
+    sid = subprocess.check_output("servald id self", shell=True)
     return sid.split('\n')[2]
     
 def getConversationsSids():
-    sids = subprocess.check_output(SERVALD_BIN + " meshms list conversations " + getOwnSid(), shell=True)
-    return [x.split(':')[1] for x in sids.split('\n')[2:-1]]
+    conv_json = subprocess.check_output('/serval-tests/meshms-list-conv-curl ' + getOwnSid(), shell=True)
+    parsed_conv_json = json.loads(conv_json)
+    other_sid_list = [parsed_conv_json['rows'][i][2] for i in range(len(parsed_conv_json['rows']))]
+    return other_sid_list
     
 def printAllConversations():
     now = int(time.time())
     own_sid = getOwnSid()
     other_sids = getConversationsSids()
     for other_sid in other_sids:
-        conversation = (subprocess.check_output(SERVALD_BIN + " meshms list messages " + own_sid + " " + other_sid, shell=True)).split('\n')
-        for conversation_message in conversation[2:-1]:
-        	send_time = now - int(conversation_message.split(':')[2])
-        	message = conversation_message.split(':')[4]
-        	direction = conversation_message.split(':')[3]
+        conversation_json = subprocess.check_output("/serval-tests/meshms-list-mess-curl " + own_sid + " " + other_sid, shell=True)
+        parsed_conversation_json = json.loads(conversation_json)
+        for conversation_message in parsed_conversation_json['rows']:
+        	send_time = conversation_message[-2]
+        	message = conversation_message[5]
+        	direction = conversation_message[0]
         	
         	if direction == '>':
         		print str(send_time) + ',' + own_sid + ',' + other_sid + ',' + message
